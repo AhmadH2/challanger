c522d9aa56d1b0ff7cc710d33c1eba24843eea4b
Ahmad Horyzat <171059@ppu.edu.ps>
2020-11-15 20:06:36 +0200

    build add topic loic

--- db.js
+++ db.js
@@ -2,6 +2,22 @@ let topics = ['topic1', 'topic2', 'topic3'];
 let quizes = [];
 let questions = [];
 
+class Question{
+    constructor(title, topic, options) {
+        this.title = title;
+        this.topic = topic;
+        this.options = options;
+    }
+}
+
+class Quize{
+    constructor(title, topic, numOfQuestions) {
+        this.title = title;
+        this.topic = topic;
+        this.numOfQuestions = numOfQuestions;
+    }
+}
+
 if(localStorage.getItem('topics')){
     topics = JSON.parse(localStorage.topics);
 }
@@ -15,11 +31,35 @@ if (localStorage.getItem('questions')) {
 }
 
 let addTopic= () => {
-    topic = document.getElementById('topicTextBox').value;
+    topic = document.getElementById('topicTitle').value;
     topics.push(topic);
     localStorage.setItem("topics", JSON.stringify(topics));
-    document.getElementById('topicTextBox').value = '';
+    document.getElementById('topicTitle').value = '';
 }
+
+let addQuize = () => {
+    let title = document.getElementById('quizeTitle').value;
+    let topicId = document.getElementById('topicsOfQuiz').selectedIndex;
+    let topic = document.getElementById("topicsOfQuiz").options[topicId].innerHTML;
+    let numOfQuestions = document.getElementById('numOfQuestions').value;
+    quizes.push(new Quize(title, topic, numOfQuestions));
+    localStorage.setItem("quizes", JSON.stringify(quizes));
+    document.getElementById('quizeTitle').value = '';
+    document.getElementById('numOfQuestions').value = '';
+}
+
+let addQuestion = () => {
+    let title = document.getElementById('questionTitle').value;
+    let topicId = document.getElementById('topicsOfQuestion').selectedIndex;
+    let topic = document.getElementById("topicsOfQuestion").options[majorId].innerHTML;
+    let numOfQues = document.getElementById()
+    topics.push(new Question(title, topic));
+    localStorage.setItem("topics", JSON.stringify(topics));
+    document.getElementById('topicTitle').value = '';
+}
+
+
+
 
 let readTopics = () => {
     let topicsHtml = '';
@@ -35,8 +75,8 @@ let showTopics = (id) => {
 
 let readQuizes = () => {
     let quizsHtml = '';
-    topics.forEach(quiz => quizsHtml += `<li>${quiz}</li>`);
-    document.getElementById('topicsList').innerHTML = quizsHtml;
+    quizes.forEach(quiz => quizsHtml += `<li>${quiz.title}, ${quiz.topic}</li>`);
+    document.getElementById('quizesList').innerHTML = quizsHtml;
 }
 
 let readQuestions = () => {
