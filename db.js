1f204376a41d3b5621058d0fa1a15511d80ce878
Ahmad Horyzat <171059@ppu.edu.ps>
2020-11-15 21:17:26 +0200

    build add question logic

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
+
+
 let topics = ['topic1', 'topic2', 'topic3'];
 let quizes = [];
 let questions = [];
+let options = [0,1];
 
 class Question{
     constructor(title, topic, options) {
@@ -51,11 +54,18 @@ let addQuize = () => {
 let addQuestion = () => {
     let title = document.getElementById('questionTitle').value;
     let topicId = document.getElementById('topicsOfQuestion').selectedIndex;
-    let topic = document.getElementById("topicsOfQuestion").options[majorId].innerHTML;
-    let numOfQues = document.getElementById()
-    topics.push(new Question(title, topic));
-    localStorage.setItem("topics", JSON.stringify(topics));
-    document.getElementById('topicTitle').value = '';
+    let topic = document.getElementById("topicsOfQuestion").options[topicId].innerHTML;
+    let opt = [];
+    for(let i=0; i<options.length; i++) {
+        opt[i] = document.getElementById(`opt${i}`).value;
+    }
+    questions.push(new Question(title, topic, opt));
+    localStorage.setItem("questions", JSON.stringify(questions));
+    document.getElementById('questionTitle').value = '';
+    for (let i = 0; i < options.length; i++) {
+        document.getElementById(`opt${i}`).value = '';
+    }
+    console.log(questions[0].title);
 }
 
 
@@ -81,6 +91,31 @@ let readQuizes = () => {
 
 let readQuestions = () => {
     let questionHtml = '';
-    topics.forEach(question => questionHtml += `<li>${question}</li>`);
-    document.getElementById('topicsList').innerHTML = questionHtml;
+    questions.forEach(question => questionHtml += `<li>Title: ${question.title},Topic: ${question.topic}, Options: ${question.options}</li>`);
+    document.getElementById('questionsList').innerHTML = questionHtml;
+}
+
+let showOptions = () => {
+    let optionsHtml = '';
+    for(let i=0; i<options.length; i++) {
+        optionsHtml += `<div>
+                        <input type="text" class="text-box" id="opt${i}">
+                        <button class="option-btn-add" onclick=addOption()>+</button>
+                        <button class="option-btn-remove" onclick=deleteOption()>X</button>
+                    </div>`;
+    }
+    document.getElementById('optionsTextBoxes').innerHTML = optionsHtml;
+}
+
+let addOption = () => {
+    options.push(options.length);
+    showOptions();
+}
+
+let deleteOption = () => {
+    if(options.length > 0) {
+        options.pop();
+        showOptions();
+    }
+
 }
